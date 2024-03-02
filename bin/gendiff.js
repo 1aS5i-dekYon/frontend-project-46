880dca2cd62cce14c0f9b2f8f0cfce547f87ab62
da.lasitsa <neolakalasua641@gmail.com>
2024-03-03 00:58:54 +0200

    SETUP & ADD: gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+//   .option('-V, --version', 'output the version number')
+//   .option('-h, --help', 'output usage information')
+//   .action((program) => console.log(program));
+
+// program.command
+  
+
+program.parse();
