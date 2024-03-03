b7eb98e97462290d36ad5b37ac4b6193cb0aba13
da.lasitsa <neolakalasua641@gmail.com>
2024-03-03 13:37:54 +0200

    UPDATE: gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-//   .option('-V, --version', 'output the version number')
-//   .option('-h, --help', 'output usage information')
-//   .action((program) => console.log(program));
-
-// program.command
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
   
 
 program.parse();
