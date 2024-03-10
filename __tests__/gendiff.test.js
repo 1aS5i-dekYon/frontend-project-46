b38263db9cceab3c0f6b1c3d3d2086651abab9a6
da.lasitsa <neolakalasua641@gmail.com>
2024-03-10 19:50:22 +0200

    ADD: .npmrc, Makefile __tests/ &UPDATE: __fixtures__/

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,35 @@
+import { test, expect } from '@jest/globals';
+import { genDiff } from '../src/index.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('test1: gendiff file1.json file2.json', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+    const result = genDiff(filepath1, filepath2);
+    expect(result).toEqual(`{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}`);
+})
+
+test('test2: gendiff empty-file.json file2.json', () => {
+    const filepath1 = getFixturePath('empty-file.json');
+    const filepath2 = getFixturePath('file2.json');
+    const result = genDiff(filepath1, filepath2);
+    expect(result).toEqual(`{\n  + host: hexlet.io\n  + timeout: 20\n  + verbose: true\n}`);
+})
+
+test('test3: gendiff empty-file.json empty-file.json', () => {
+    const filepath = getFixturePath('empty-file.json');
+    const result = genDiff(filepath, filepath);
+    expect(result).toEqual(`{\n  \n}`);
+})
+
+test('test4: gendiff file1.json file1.json', () => {
+    const filepath = getFixturePath('file1.json');
+    const result = genDiff(filepath, filepath);
+    expect(result).toEqual(`{\n    follow: false\n    host: hexlet.io\n    proxy: 123.234.53.22\n    timeout: 50\n}`);
+})
